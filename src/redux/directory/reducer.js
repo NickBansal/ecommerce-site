e9251794fad78dbeaf3acad743ba77d9cc3f49b3
Nick Bansal <extreme_wired@msn.com>
2020-04-30 12:01:01 +0100

    updated the directory reducer and types

--- src/redux/directory/reducer.js
+++ src/redux/directory/reducer.js
@@ -4,11 +4,30 @@ import directoryTypes from './types';
 
 const INITIAL_STATE = {
 	data: null,
-	sections: OPTIONS
+	sections: OPTIONS,
+	isFetching: false,
+	errorMessage: undefined
 };
 
 const directoryReducer = (state = INITIAL_STATE, actions) => {
 	switch (actions.type) {
+		case directoryTypes.FETCH_COLLECTIONS_START:
+			return {
+				...state,
+				isFetching: true
+			};
+		case directoryTypes.FETCH_COLLECTIONS_SUCCESS:
+			return {
+				...state,
+				data: actions.payload,
+				isFetching: false
+			};
+		case directoryTypes.FETCH_COLLECTIONS_FAILURE:
+			return {
+				...state,
+				errorMessage: actions.payload,
+				isFetching: false
+			};
 		case directoryTypes.GET_CURRENT_DATA:
 			return {
 				...state,
