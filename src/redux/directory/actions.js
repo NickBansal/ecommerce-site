12c8e8bb07bb213cf156cceade3ae3d87924e70b
Nick Bansal <extreme_wired@msn.com>
2020-04-30 12:18:05 +0100

    added in fetch collections actions

--- src/redux/directory/actions.js
+++ src/redux/directory/actions.js
@@ -1,8 +1,40 @@
 import directoryTypes from './types';
+import { firestore } from '../../firebase/utils';
+import convertCollectionsToMap from '../../firebase/convertCollectionsToMap';
 
 export const setCurrentData = data => ({
 	type: directoryTypes.GET_CURRENT_DATA,
 	payload: data
 });
 
-export const user = () => {};
+export const fetchCollectionsStart = () => ({
+	type: directoryTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsSuccess = data => ({
+	type: directoryTypes.FETCH_COLLECTIONS_SUCCESS,
+	payload: data
+});
+
+export const fetchCollectionsFailure = errorMessage => ({
+	type: directoryTypes.FETCH_COLLECTIONS_FAILURE,
+	payload: errorMessage
+});
+
+export const fetchCollectionsStartAsync = () => {
+	return dispatch => {
+		const collectionRef = firestore.collection('collections');
+
+		dispatch(fetchCollectionsStart());
+
+		collectionRef
+			.get()
+			.then(snapshot => {
+				const collectionsMap = convertCollectionsToMap(snapshot);
+				dispatch(fetchCollectionsSuccess(collectionsMap));
+			})
+			.catch(error => {
+				dispatch(fetchCollectionsFailure(error));
+			});
+	};
+};
