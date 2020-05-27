2ff543a347b44cd9952fe8ccfd9653cf47983d02
Nick Bansal <extreme_wired@msn.com>
2020-05-27 14:01:31 +0100

    hooked up the backend to the front end

--- client/src/components/StripeButton/index.js
+++ client/src/components/StripeButton/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import { useDispatch } from 'react-redux';
+import axios from 'axios';
 import { clearCart } from '../../redux/cart/actions';
 
 const StripeButton = ({ price }) => {
@@ -9,9 +10,21 @@ const StripeButton = ({ price }) => {
 	const priceForStripe = price * 100;
 	const publishableKey = 'pk_test_CXDPdTpBGh3d5iaS8yHnIdFG00Y0v2XEN0';
 
-	const onToken = () => {
-		alert('Payment successful');
-		dispatch(clearCart());
+	const onToken = (token) => {
+		axios({
+			url: 'payment',
+			method: 'POST',
+			data: {
+				amount: priceForStripe,
+				token
+			}
+		}).then(() => {
+			alert('Payment successful');
+			dispatch(clearCart());
+		}).catch((error) => {
+			console.log('Payment error ', JSON.parse(error));
+			alert('There was an issue with your payment, please try again')
+		})
 	};
 
 	return (
