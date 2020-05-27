01ef8c79b77f00658eab7f191a594beebb6cd875
Nick Bansal <extreme_wired@msn.com>
2020-05-27 13:45:05 +0100

    set up backend route for stripe payments

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,4 +31,20 @@ app.listen(port, (error) => {
     throw (error);
   }
   console.log(`Running on port ${port}`);
+});
+
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'gbp',
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
